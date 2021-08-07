9e12862ff1218ad7f04d7d0b82b4b36b0d45e904
Анастасия Кудрявцева <anastasiakudravceva@MacBook-Air-Anastasia.local>
2021-08-07 21:02:58 +0300

    moved css file to the stite folder and created linc for css file

--- js/viewPaje.js
+++ js/viewPaje.js
@@ -1,22 +1,41 @@
+function f() {
 
 
-    function f() {
-        var data = document.getElementById("main");
-        data.parentNode.removeChild(data);
-        var main = document.createElement("main");
-        main.style.background = "green";
+    var data = document.getElementById("main");
+    data.parentNode.removeChild(data);
+    var main = document.createElement("main");
+    var input1 = document.createElement("input");
+    input1.type = "text";
+    var input2 = document.createElement("input");
+    input2.type = "text";
+    var input3 = document.createElement("input");
+    input3.type = "text";
 
-        main.id = "main";
-        document.body.firstElementChild.after(main);
-    }
+    main.append(input1);
+    input1.placeholder = "name: ";
+    input1.className = "form-control input-style";
 
-    function f2() {
-        var data = document.getElementById("main");
-        data.parentNode.removeChild(data);
-        var main = document.createElement("main");
-        main.style.background = "red";
+    main.append(input2);
+    input2.placeholder = "mail: ";
+    input2.className = "form-control input-style";
 
-        main.id = "main";
-        document.body.firstElementChild.after(main);
+    main.append(input3);
+    input3.placeholder = "pass: ";
+    input3.className = "input-style form-control";
 
-    }
+    main.style.background = "green";
+
+    main.id = "main";
+    document.body.firstElementChild.after(main);
+}
+
+function f2() {
+    var data = document.getElementById("main");
+    data.parentNode.removeChild(data);
+    var main = document.createElement("main");
+    main.style.background = "red";
+
+    main.id = "main";
+    document.body.firstElementChild.after(main);
+
+}
