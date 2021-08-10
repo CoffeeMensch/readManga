8c9229cff6400805c4c810567902c3a17c0e5eff
Анастасия Кудрявцева <anastasiakudravceva@Air-Anastasia.Dlink>
2021-08-10 13:13:56 +0300

    created main pane and chaainjed stiles

--- js/viewPaje.js
+++ js/viewPaje.js
@@ -1,6 +1,5 @@
 function f() {
 
-
     var data = document.getElementById("main");
     data.parentNode.removeChild(data);
     var main = document.createElement("main");
@@ -23,19 +22,8 @@ function f() {
     input3.placeholder = "pass: ";
     input3.className = "input-style form-control";
 
-    main.style.background = "green";
+    main.style.background = "#1f0a07";
 
     main.id = "main";
     document.body.firstElementChild.after(main);
-}
-
-function f2() {
-    var data = document.getElementById("main");
-    data.parentNode.removeChild(data);
-    var main = document.createElement("main");
-    main.style.background = "red";
-
-    main.id = "main";
-    document.body.firstElementChild.after(main);
-
 }
