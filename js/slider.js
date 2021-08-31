728f4edcfa398c6144f7aebb90786c4f5fa1d684
Анастасия Кудрявцева <anastasiakudravceva@MacBook-Air-Anastasia.local>
2021-08-31 11:14:37 +0300

    fixed slider

--- js/slider.js
+++ js/slider.js
@@ -1,26 +1,50 @@
 let canvas = document.getElementById('canvas');
-canvas.addEventListener('mousemove', move);
-let mouseClickerLeft = 0;
-let mousClickerRight = 0;
+canvas.addEventListener('mousemove', saveXY);
 
-function move(event) {
-    console.log("x = " + event.clientX);
-    console.log("y = " + event.clientY);
+let x = 0;
+let time = 1000;
 
-    let con = canvas.getBoundingClientRect().width / 2;
-    if (event.clientX < con) {
-        mouseClickerLeft++;
-        if (mouseClickerLeft >= 50) {
-            leftSlide();
-            mouseClickerLeft = 0;
-        }
-    } else {
-        mousClickerRight++;
-        if (mousClickerRight >= 50) {
-            rightSlide();
-            mousClickerRight = 0;
-        }
+
+//setInterval(move, time);//move each sekond
+
+
+function myFunction() {
+    move();
+    setTimeout(myFunction, time);
+}
+
+setTimeout(myFunction, time);
+
+function saveXY(event) {
+    x = event.clientX;
+}
+
+function speed() {
+    var width = canvas.getBoundingClientRect().width;
+    var onePercent = width / 100;
+
+    if (x < onePercent * 20 || x > onePercent * 80) {
+        time = 200;
+    } else if (x < onePercent * 30 && x > onePercent * 20 || x > onePercent * 60 && x < onePercent * 70) {
+            time = 400;
+    } else if (x < onePercent * 40 && x > onePercent * 30 || x > onePercent * 70 && x < onePercent * 80) {
+        time = 700;
     }
+}
+
+
+function move() {
+
+    speed();
+    let con = canvas.getBoundingClientRect().width / 2;//1400
+    if (x < con) {
+        leftSlide();
+
+    } else {
+
+        rightSlide();
+    }
+
 
 }
 
